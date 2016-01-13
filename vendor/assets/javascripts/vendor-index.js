34557a86e62debadfd2f0410ffc9392ed4038946
activitar-gdt <gerhard@activitar.com>
2016-01-13 10:01:53 +0200

    production pipeline

--- vendor/assets/javascripts/vendor-index.js
+++ vendor/assets/javascripts/vendor-index.js
@@ -1,21 +1,23 @@
-/**
- * Created by g on 2016/01/08.
- */
-
+// -- JS Global Compulsory
 //= require plugins/jquery/jquery-migrate.min
+
+// -- JS Implementing Plugins
+
 //= require plugins/back-to-top
 //= require plugins/smoothScroll
+//= require plugins/sky-forms-pro/skyforms/js/jquery-ui.min
+//= require plugins/sky-forms-pro/skyforms/js/jquery.validate.min
+//= require plugins/sky-forms-pro/skyforms/js/jquery.maskedinput.min
+//= require plugins/scrollbar/js/jquery.mCustomScrollbar.concat.min
 //= require plugins/fancybox/source/jquery.fancybox.pack
 //= require plugins/jquery.parallax
+//= require plugins/master-slider/masterslider/masterslider.min.js
+//= require plugins/master-slider/masterslider/jquery.easing.min.js
 //= require plugins/counter/waypoints.min
 //= require plugins/counter/jquery.counterup.min
 //= require plugins/revolution-slider/rs-plugin/js/jquery.themepunch.tools.min
 //= require plugins/revolution-slider/rs-plugin/js/jquery.themepunch.revolution.min
 //= require plugins/owl-carousel/owl-carousel/owl.carousel.min
-//= require plugins/scrollbar/js/jquery.mCustomScrollbar.concat.min
-//= require plugins/sky-forms-pro/skyforms/js/jquery-ui.min
-//= require plugins/sky-forms-pro/skyforms/js/jquery.maskedinput.min
-//= require plugins/sky-forms-pro/skyforms/js/jquery.validate.min
 
 
 // JS Customization
@@ -23,9 +25,11 @@
 
 // JS Page Level
 //= require js/app
-//= require js/plugins/revolution-slider
+//= require js/plugins/master-slider-fw
 //= require js/plugins/owl-carousel
 //= require js/plugins/fancy-box
 //= require js/plugins/masking
-// js/plugins/datepicker            Use the one on app/assets/javascripts
 //= require js/plugins/validation
+
+// js/plugins/datepicker            Use the one on app/assets/javascripts
+
